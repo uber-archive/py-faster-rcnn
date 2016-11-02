327a7b446aa9c609877b7c754a71357d9e6b96f1
peizha@uber.com <peizha@uber.com>
2016-11-02 16:46:19 -0600

    some code change to make it easy to train with iei data.

--- lib/fast_rcnn/nms_wrapper.py
+++ lib/fast_rcnn/nms_wrapper.py
@@ -5,16 +5,12 @@
 # Written by Ross Girshick
 # --------------------------------------------------------
 
-from fast_rcnn.config import cfg
-# from nms.gpu_nms import gpu_nms
-from nms.cpu_nms import cpu_nms
+from processing.utility.frcnn.nms.py_cpu_nms import py_cpu_nms
 
 def nms(dets, thresh, force_cpu=False):
     """Dispatch to either CPU or GPU NMS implementations."""
 
     if dets.shape[0] == 0:
         return []
-    # if cfg.USE_GPU_NMS and not force_cpu:
-    #     return gpu_nms(dets, thresh, device_id=cfg.GPU_ID)
     else:
-        return cpu_nms(dets, thresh)
+        return py_cpu_nms(dets, thresh)
